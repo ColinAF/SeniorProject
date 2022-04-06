6f1f4d046867bdf997412335766f0d8fb56c2b6e
Colin Frisch <cafrisch98@gmail.com>
2022-04-06 01:38:17 -0700

    Stub for a visualization module

---
+++ Model_Code/Object_Detection/visualization.py
@@ -0,0 +1,22 @@
+### Visualizations ###
+
+### External Imports ###
+import matplotlib.pyplot as plt 
+### External Imports ###
+
+### Local Imports ### 
+### Local Imports ### 
+
+def displayImage():
+    pass 
+# # Display image and label.
+# train_features, train_labels = next(iter(train_dataloader))
+# # image shape is [batch_size, 3 (due to RGB), height, width]
+# print(train_features.shape)
+# img = transforms.ToPILImage()(train_features[0])
+# plt.imshow(img)
+# plt.show()
+# #print(train_labels)
+
+
+### Visualizations ###

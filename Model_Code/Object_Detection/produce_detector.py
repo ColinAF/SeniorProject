7fc9f988fe0dcd99244bc1296eb7111dee4b0af7
Colin Frisch <cafrisch98@gmail.com>
2022-04-07 01:28:47 -0700

    Swapped backbone

--- Model_Code/Object_Detection/produce_detector.py
+++ Model_Code/Object_Detection/produce_detector.py
@@ -1,10 +1,3 @@
-### NOTES ###
-# - Add a module that keeps track of hyperparams and paths in json
-# - Add more data transforms!!! 
-# - Files for {train,validate,model,visualizations}
-# - Super helpful tutorial: https://medium.com/fullstackai/how-to-train-an-object-detector-with-your-own-coco-dataset-in-pytorch-319e7090da5
-### NOTES ###
-
 ### Produce Detector ###
 
 ### External Imports ###
@@ -14,12 +7,10 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 ### All this is still copied from the tutorial for testing! - Not Mine ###
 
+# Probably better to make this an object and not a function?
 def get_model(num_classes):
-    # load an instance segmentation model pre-trained pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    # get number of input features for the classifier
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     return model

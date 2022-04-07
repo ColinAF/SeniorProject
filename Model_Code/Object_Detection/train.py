0bf511a8822b31c2bb2a7fa23d5530996a08bd5b
Colin Frisch <cafrisch98@gmail.com>
2022-04-06 21:27:38 -0700

    Improved training timer

--- Model_Code/Object_Detection/train.py
+++ Model_Code/Object_Detection/train.py
@@ -1,3 +1,13 @@
+### NOTES ###
+# - Add a module that keeps track of hyperparams and paths in json
+# - Add more data transforms!!! 
+# - Files for {train,test/validate,model,visualizations}
+# - Super helpful tutorial: https://medium.com/fullstackai/how-to-train-an-object-detector-with-your-own-coco-dataset-in-pytorch-319e7090da5
+# - Export timeings to a CSV file!
+### NOTES ###
+
+
+
 ### Training Script ###
 
 ### External Imports ###
@@ -26,7 +36,7 @@ def collate_fn(batch):
 
 train_batch_size = 1
 
-#Put the data in a dataloader
+# Add Data Transforms
 train_dataloader = DataLoader(train_dataset, 
                               batch_size=train_batch_size, 
                               shuffle=True,
@@ -36,7 +46,6 @@ train_dataloader = DataLoader(train_dataset,
 # select device (whether GPU or CPU)
 #device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 device = torch.device('cpu') # Training uses way too much GPU atm see if I can manage this better!
-
 
 num_classes = 3
 num_epochs = 10
@@ -51,15 +60,24 @@ optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
 
 len_dataloader = len(train_dataloader)
 
+# Add options for minutes as well
+def time_elapsed(t_finish, t_start):
+    t_elapsed = time.gmtime((t_finish - t_start))
+    return (time.strftime("%H:%M:%S", t_elapsed))
+
 print("Start Training!")
 
-t0 = time.time()
+t_start = time.time()
+t_last_epoch = t_start
 
 for epoch in range(num_epochs):
     model.train()
     i = 0
 
-    print("Epoch: " + str(epoch))
+    t_epoch = time.time()
+    print("Epoch: " + str(epoch+1) +  " Time in epoch: " + time_elapsed(t_epoch, t_last_epoch))
+
+    t_last_epoch = t_epoch
 
     for imgs, annotations in train_dataloader :
         i += 1
@@ -72,11 +90,10 @@ for epoch in range(num_epochs):
         losses.backward()
         optimizer.step()
 
-        print(f'Iteration: {i}/{len_dataloader}, Loss: {losses}')
+        print(f'Epoch: {epoch+1} Iteration: {i}/{len_dataloader}, Loss: {losses}')
 
-t1 = time.time()
-print("Took " + (t1 - t0) + " seconds to train!")
-
+t_finish = time.time()
+print("Took " + time_elapsed(t_finish, t_start) + " seconds to train!")
 
 
 ### All this is still copied from the tutorial for testing! - Not Mine ###

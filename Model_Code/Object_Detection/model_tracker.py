29ca9a738cf737aab61483c5d1667bc49ab331de
Colin Frisch <cafrisch98@gmail.com>
2022-04-08 16:25:45 -0700

    feat: Stubbed out an object to track models

---
+++ Model_Code/Object_Detection/model_tracker.py
@@ -0,0 +1,18 @@
+### Model Tracker ###
+
+'''
+A way to keep track of trained models
+- Add a unique id for each model
+- Bunch it up with its stats and a copy of its hyperparams.json
+'''
+
+class ModelTracker():
+    def __init__(model):
+        pass
+    def save_model():
+        pass
+    def load_model(id):
+        pass
+    def assign_id():
+        pass
+### Model Tracker ###
